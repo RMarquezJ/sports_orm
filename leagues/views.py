79c2b19d675019c59321fe70968c835b77b54fb0
Rodrigo Marquez <rod.m89@gmail.com>
2021-08-16 20:41:47 -0400

    all excercises done

--- leagues/views.py
+++ leagues/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 from .models import League, Team, Player
+from django.db.models import Count 
 
 from . import team_maker
 
 def index(request):
-
+  
   context = {
 		"leagues": League.objects.all(),
 		"teams": Team.objects.all(),
@@ -32,6 +33,7 @@ def index(request):
 def index2(request):
 
   context = {
+
 		"leagues": League.objects.all(),
 
 		"teams": Team.objects.all(),
@@ -40,13 +42,11 @@ def index2(request):
 
     "atlanticcityconf": Team.objects.filter(league__name = "Atlantic Soccer Conference"),
 
-    "bospenplayers": Player.objects.filter(curr_team__location="Boston") &
- Player.objects.filter(curr_team__team_name="Penguins"),
- 
+    "bospenplayers": Player.objects.filter(curr_team__location="Boston", curr_team__team_name="Penguins"),
+
     "intcolbasecon": Player.objects.filter(curr_team__league__name="International Collegiate Baseball Conference"),
 
-    "lopezfootballplayer": Player.objects.filter(curr_team__league__name="American Conference of Amateur Football") &
-Player.objects.filter(last_name="Lopez"),
+    "lopezfootballplayer": Player.objects.filter(curr_team__league__name="American Conference of Amateur Football").filter(last_name="Lopez"),
 
     "footballplayers": Player.objects.filter(curr_team__league__sport="Football"),
 
@@ -54,7 +54,7 @@ Player.objects.filter(last_name="Lopez"),
 
     "sophialeagueplayer": League.objects.filter(teams__all_players__first_name="Sophia"),
 
-    "floresnot": Player.objects.filter(last_name='Flores').exclude(curr_team__team_name='Roughriders') & Player.objects.exclude(curr_team__location='Washington'),
+    "floresnot": Player.objects.filter(last_name='Flores').exclude(curr_team__team_name='Roughriders', curr_team__location='Washington'),
 
     "samuelevansteams": Team.objects.filter(curr_players__first_name='Samuel', curr_players__last_name='Evans', all_players__first_name='Samuel', all_players__last_name='Evans'),
 
@@ -62,11 +62,13 @@ Player.objects.filter(last_name="Lopez"),
 
     "formerwichitavikings": Player.objects.filter(all_teams__team_name='Vikings', all_teams__location='Wichita').exclude(curr_team__team_name='Vikings', curr_team__location='Wichita'),
 
-    "orderbyname": Team.objects.all().order_by('-team_name'),
-    "playercooper": Player.objects.filter(last_name='Cooper'),
-    "playerjoshua": Player.objects.filter(first_name='Joshua'),
-    "cooperbutnotjoshua": Player.objects.filter(last_name='Cooper').exclude(first_name='Joshua'),
-    "alexanderorwyatt": Player.objects.filter(first_name ='Alexander')|Player.objects.filter(first_name='Wyatt'),
+    "formerteamsjacobgray": Team.objects.filter(all_players__first_name='Jacob', all_players__last_name = 'Gray'). exclude(team_name='Colts', location = 'Oregon'),
+
+    "joshuabaseleague": Player.objects.filter(first_name='Joshua', all_teams__league__name='Atlantic Federation of Amateur Baseball Players'),
+
+    "twelveplayers": Team.objects.annotate(Count('curr_players')).annotate(Count('all_players')).filter(curr_players__count__gte=12).filter(all_players__count__gte=12),
+
+    "allplayers": Player.objects.all().annotate(Count('all_teams')).order_by('all_teams__count'),
 	}
 
   return render(request, "leagues/index2.html", context)
